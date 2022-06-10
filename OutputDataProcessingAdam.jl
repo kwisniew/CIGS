3ef981412fc11d9e47b7945633a55481ef923f9f
kwisniew <kon.wu888@gmail.com>
2022-06-10 18:31:44 +0200

    cleaning up 1) display 3 plot: C vs. V, 1/C^2 vs. V and linear fir to the plot 1/C^2 vs V 2) Do not chenge V values (line 23) 3) new function names (more specific) 4) move calculations of build in potential to "cal_dopind_and_build_in_pot"

--- OutputDataProcessingAdam.jl
+++ OutputDataProcessingAdam.jl
@@ -19,8 +19,7 @@ staticCapacitance        = CSV.read(filepath  , DataFrame; header=false)
 biasValues               = CSV.read(filepath2 , DataFrame; header=false) 
 
 #creating data for plots
-
-y    = abs.(staticCapacitance.Column1*10^9).^-2
+y    = abs.(staticCapacitance.Column1).^-2
 bias = biasValues.Column1#.+biasValues.Column1[2]/2
 bias = bias[1:100]
 bias = abs.(bias)
@@ -29,23 +28,20 @@ data = DataFrame(X=bias[1:95],Y=y[1:95])
 ols = lm(@formula(Y ~ X), data)
 coef(ols)
 
-function display() #function to display plots
+function display_capacitance() #function to display plots
 
     plot1=plot(biasValues.Column1[1:95],abs.(staticCapacitance.Column1[1:95])*10^9, reuse=false, xlabel = "U[V]", ylabel="C[nF]", seriestype = :scatter)
-    plot2=plot(bias[1:95],y[1:95], reuse=false, xlabel = "U[V]", ylabel="1/C[nF]^2", seriestype = :scatter)
+    plot2=plot(bias[1:95],y[1:95]*10^(-18), reuse=false, xlabel = "U[V]", ylabel="1/C[nF]^2", seriestype = :scatter)
     
 
     model(x) = coef(ols)[1] + coef(ols)[2] * x
-    #plot!(data.X, model.(data.X), lw=3)
-    #scatter(data.X, data.Y)
-    plot3=plot(bias[1:95],y[1:95], reuse=false, xlabel = "U[V]", ylabel="1/C[nF]^2", seriestype = :scatter, title="Linear fit:")
-    plot!(data.X, model.(data.X), lw=3)
-    #plot3= plot(data.X, model.(data.X), legend=false, reuse = false, xlabel = "Voltage", ylabel="1/C[nF]^2", title="From linear fit:")
+    plot3=plot(bias[1:95],y[1:95]*10^(-18), reuse=false, xlabel = "U[V]", ylabel="1/C[nF]^2", seriestype = :scatter, title="Linear fit:")
+    plot!(data.X, model.(data.X)*10^(-18), lw=3)
 
     plot(plot1, plot2, plot3, layout = (3,1), legend=false) 
     
 end
- function calculate() #function for calculations 
+ function cal_doping_and_build_in_pot() #function for calculations 
     eps0       = 8.85*10^-14 * u"F/cm"
     epsRelativ = 13.6 
     S          = 0.5*0.5
@@ -55,7 +51,7 @@ end
     return N_derived, build_in_potential
  end
 
- function print() #test function for printing values
+ function print_liner_fit_coefs() #test function for printing values
     printer = coef(ols)
     return printer
  end
