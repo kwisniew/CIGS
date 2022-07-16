95c5ff13e6f9ef4c2e5500dfbb070be0e1f99d04
kwisniew <kon.wu888@gmail.com>
2022-07-16 16:16:49 +0200

    files for testing time-domain calculations

---
+++ Structure_Schottky_CIGS/transient_case/esitimation_of_time.jl
@@ -0,0 +1,23 @@
+using ChargeTransport
+using Unitful
+
+mup_CIGS = 25* u"(cm^2) / (V * s)"
+Na       = 1.0e15 *u"1/(cm^3)"
+
+#estimation of depleteion region (you can read it from plot od energy band in equilibrium)
+W = 0.75 * u"μm"
+
+w_device = 1.0 *u"cm"  # width of device
+z_device = 1.0 *u"cm"  # depth of device
+S        = w_device*z_device #1cm2
+
+Q = 1.602176565*10^-19 * u"C"
+ρ = 1/(Q*Na*mup_CIGS)
+
+R= ρ * W / S
+
+#I'm approximating the value of C from the CV plot: C was taken for V=0
+C = 1.1e-8 *u"F"
+
+τ = R*C
+upreferred(τ)
